4c8b44a1c104c94c77708e1b5e3b6b888fb1e883
Agilatech <scott@agilatech.com>
2017-08-09 15:59:21 -0600

    Changed to class syntax

--- mock-scout.js
+++ mock-scout.js
@@ -3,51 +3,50 @@ const options = require('./options');
 
 const Scout = require('zetta-scout');
 const Mock = require('./mock');
-const util = require('util');
 
-const MockScout = module.exports = function(opts) {
-    
-  // see if any of the options were overridden in the server
+module.exports = class MockScout extends Scout {
 
-  if (typeof opts !== 'undefined') {
-    // copy all options defined in the server
-    for (const key in opts) {
-      if (typeof opts[key] !== 'undefined') {
-        options[key] = opts[key];
+  constructor(opts) {
+
+    super();
+
+    if (typeof opts !== 'undefined') {
+      // copy all options defined in the server
+      for (const key in opts) {
+        if (typeof opts[key] !== 'undefined') {
+          options[key] = opts[key];
+        }
       }
     }
+
+    if (options.name === undefined) { options.name = "MOCK" }
+    this.name = options.name;
+
+    this.mock = new Mock(options);
+
   }
 
-  if (options.name === undefined) { options.name = "MOCK" }
-  this.name = options.name;
-
-  this.mock = new Mock(options);
-
-  Scout.call(this);
-};
-
-util.inherits(MockScout, Scout);
-
-MockScout.prototype.init = function(next) {
-
-  const query = this.server.where({name: this.name});
+  init(next) {
+    const query = this.server.where({name: this.name});
   
-  const self = this;
-  this.server.find(query, function(err, results) {
-    if (!err) {
-      if (results[0]) {
-        self.provision(results[0], self.mock);
-        self.server.info('Provisioned known device ' + self.name);
-      } else {
-        self.discover(self.mock);
-        self.server.info('Discovered new device ' + self.name);
+    const self = this;
+
+    this.server.find(query, function(err, results) {
+      if (!err) {
+        if (results[0]) {
+          self.provision(results[0], self.mock);
+          self.server.info('Provisioned known device ' + self.name);
+        } else {
+          self.discover(self.mock);
+          self.server.info('Discovered new device ' + self.name);
+        }
       }
-    }
-    else {
-      self.server.error(err);
-    }
-  });
+      else {
+        self.server.error(err);
+      }
+    });
 
-  next();
+    next();
+  }
 
-};
+}
